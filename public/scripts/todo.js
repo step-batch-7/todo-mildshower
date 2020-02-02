abbda758b9b6659d41695361161731c9dee0f83c
Sudipta Kundu <58025792+mildshower@users.noreply.github.com>
2020-02-03 01:25:03 +0530

    added ability to show time
    now time and no of tasks left is shown

--- public/scripts/todo.js
+++ public/scripts/todo.js
@@ -1,14 +1,31 @@
+const MONTHS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June',
+  'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+const DAYS = ['Monday', 'Tuesday', 'Wednesday', 
+  'Thursday', 'Friday', 'Saturday'];
+
 class Todo {
   constructor(title, tasks){
     this.title = title;
     this.tasks = [...tasks];
+    this.time = new Date();
   }
 
   toHtml() {
     const tasksHtml = this.tasks.map(task => `<p>${task}</p>`).join('\n');
+    const day = DAYS[this.time.getDay()];
+    const month = MONTHS[this.time.getMonth()];
+    const date = this.time.getDate();
+    const year = this.time.getFullYear();
+    const time = `${day}, ${date} ${month}, ${year}`;
     return `
     <div class="todoBox">
-      <h3>${this.title}</h3>
+      <div class="todoHeader">
+        <h3>${this.title}</h3>
+        <div class="infoStrap">
+          <span class="time">${time}</span>
+          <span class="taskCount">${this.tasks.length} left<span>
+        </div>
+      </div>
       <div class="tasks">
         ${tasksHtml}
       </div>
